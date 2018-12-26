50137f69347084eb93b1f6cdca401611ada4c565
RITIKA <ritikawesome20@gmail.com>
2018-12-25 23:02:17 -0600

    deleted

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-
+const express = require('express');
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+// DB config
+const db = require('./config/keys').mongoURI;
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
-);
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('mongoDB connect'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
+// use Routes//
+app.use('./api/users', users);
+app.use('./api/posts', posts);
+app.use('./api/profile', profile);
+const PORT = process.env.PORT || 5000;
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}`));
