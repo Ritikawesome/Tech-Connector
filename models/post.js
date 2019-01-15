68caa842c5a0b1b07bcf6ebf0ffa99d3b6457e54
RITIKA <ritikawesome20@gmail.com>
2019-01-14 19:01:25 -0600

    Changes to be committed:

--- models/post.js
+++ models/post.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+//mongoose.connect("mongodb://localhost:27017/Techconnector", {
+//useNewUrlParser: true
+//});
 const Schema = mongoose.Schema;
 
 // Create Schema
