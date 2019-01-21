531ee84fabbfc15c304e423e8871f29452ff87eb
RITIKA <ritikawesome20@gmail.com>
2019-01-21 12:18:46 -0600

    added

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+// preventing manual to have the token we call that function its always attached that authrisation header
+const setAuthToken = token => {
+  if (token) {
+    // Apply to every request
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    // Delete auth header
+    delete axios.defaults.headers.common["Authorization"];
+  }
+};
+
+export default setAuthToken;
